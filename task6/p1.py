12a7880b0b91465f33f8fcb290fc0d2d57249cfb
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-14 00:03:41 +0200

    Update p1.py

--- task6/p1.py
+++ task6/p1.py
@@ -1,21 +1,21 @@
 t = int(input())
 while (t > 15 or t < 1):
-    t = int(input())
+    t = int(input())    # making sure that input dosen't exceed 15
 
 p = []
 c = 0
-for i in range(t):
+for i in range(t):  # receving numbers
     arr = str(input())
     n = int(arr)
     for i in range(len(arr)):
-        if(int(arr[i]) == 0):
+        if(int(arr[i]) == 0): #skipping zero's to avoid proplems
             continue
         else:
-            if(n % int(arr[i]) == 0):
+            if(n % int(arr[i]) == 0): # divisor
                 c += 1
-    p.append(c)
+    p.append(c) #appending results
     c = 0
 
-for i in range(len(p)):
+for i in range(len(p)): # printing result
     print(p[i])
 
