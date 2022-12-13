dbd2f3a85cc46c005cec89a57617fda97b5cf17c
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-14 00:19:37 +0200

    Update p3.py

--- task6/p3.py
+++ task6/p3.py
@@ -1,26 +1,26 @@
 import os
 
 def climbingLeaderboard(ranked, player) :
-    l = list(set(ranked))
+    l = list(set(ranked))        # deleting duplicates by using set method then sorting and reversing
     l.sort()
     l.reverse()
     li = []
     c = len(l) - 1
     for i in (player):
         b = 1
-        for j in range(c, -1, -1):
+        for j in range(c, -1, -1): # comparing player with oter ranks 
             if i < l[j]:
                 c = j
                 b = 0
                 break
         if(b == 1): 
-            li.append(1)    
+            li.append(1)    # appending his rank
         elif(b == 0):
            li.append(j + 2)
     return li
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')  # just getting input
 
     ranked_count = int(input().strip())
 
