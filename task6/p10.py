d0a6b010da49716b8ac1136bacf700deec10653c
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-14 22:42:05 +0200

    Update p10.py

--- task6/p10.py
+++ task6/p10.py
@@ -1,14 +1,14 @@
-for test in range(int(input())):
+for test in range(int(input())):  # getting every case dynamically
 
     count, remove = map(int, input().split())
     friends = map(int, input().split())
     s = []
     for i in friends:
-        while remove and s and s[-1] < i:
-            s.pop()
+        while remove and s and s[-1] < i:  # making sure not get out of range
+            s.pop()                        # removing friend after checking 
             remove -= 1
 
-        s.append(i)
+        s.append(i)                              # appending the remaining friends 
 
     print(' '.join(map(str, s)))
 
