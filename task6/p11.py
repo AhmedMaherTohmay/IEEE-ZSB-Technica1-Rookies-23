eccd18a44079a50fd46ac92cefa2442da5b070f9
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-14 22:09:13 +0200

    Update p11.py

--- task6/p11.py
+++ task6/p11.py
@@ -8,9 +8,9 @@ p = list(map(int, input().split()))
 sec = 86400
 day = 0
 c = 0
-for i in range(n):
-    c += sec - int(p[i])
-    day += 1
+for i in range(n):              # consider she want 2 sec to read the book and we have 1 sec in the first day
+    c += sec - int(p[i])        # and 1 sec in the other day to know the wanted days we add the second's of each day 
+    day += 1                    # then compare it with wanted seconeds 
     if(c >= m):
         print(day)
         break
