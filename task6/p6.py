a1f38a7df8fff673cd4d143c5b29d8e056ab0507
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-14 21:58:11 +0200

    Update p6.py

--- task6/p6.py
+++ task6/p6.py
@@ -4,9 +4,9 @@ m = len(arr2)
 li = []
 
 if(arr1[0] < arr1[1]):
-    c = (int(arr1[1]) // int(arr1[0])) * int(arr1[0])
-    b = arr1[1] - c
-else:
+    c = (int(arr1[1]) // int(arr1[0])) * int(arr1[0])   # if the rotation count is bigger than the elements in integar array 
+    b = arr1[1] - c                                     # we do this equation to get the wanted rotation 
+else:                                                   # we do the samething  when we get the angle after rotating many times
     b = arr1[1]
 
 for i in range(arr1[2]):
@@ -16,13 +16,13 @@ for i in range(arr1[2]):
 z = m - b
 p = []
 
-for i in range(z , m):
+for i in range(z , m):          # (1 , 2 , 3)  append ( 2, 3)
     p.append(arr2[i])
 
-for i in range(0 , z):
+for i in range(0 , z):           # append (1) 
     p.append(arr2[i])
 
 
-for i in range(len(li)):
+for i in range(len(li)):        # print the final list
     print(p[int(li[i])])
 
