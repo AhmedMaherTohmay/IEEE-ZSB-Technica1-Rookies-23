d5bbeafb4dbe6ce72a2dc37cba21206f33409923
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-14 21:49:36 +0200

    Update p5.py

--- task6/p5.py
+++ task6/p5.py
@@ -5,13 +5,13 @@ u = []
 for x in range(n):
     p = input()
     c, i, j = 0, 0, 0
-    while i < len(string) and j < len(p):
+    while i < len(string) and j < len(p): # finding every alphabet in hackerrenk in the given string then appending it into list
         if string[i] == p[j]:
             i += 1
         j += 1
     u.append(i)
 
-for i in range(len(u)):
+for i in range(len(u)):  # comparing the result list with hackerrank to  see if it matches
     if int(u[i]) == len(string):
         print("YES")
     else:
