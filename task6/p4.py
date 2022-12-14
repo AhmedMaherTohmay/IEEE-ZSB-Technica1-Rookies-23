e63ccae957e14f0a67e1b572b89b4421bc708946
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-14 21:40:28 +0200

    Update p4.py

--- task6/p4.py
+++ task6/p4.py
@@ -3,17 +3,17 @@ l = l.split()
 n = int(l[0])
 m = int(l[1])
 
-p = [input() for i  in range(n)]
+p = [input() for i  in range(n)]  # receving the input
 s = []
 max = 0
 
-for i in range(len(p)):
+for i in range(len(p)):  # making a loop on topics to add them to each other by using or operator then counting one's to know the people who read the topic
     j = i
     while j < n:
         first = p[i]
         second = p[j]
 
-        b = bin(int(first, 2) | int(second, 2)).count('1')
+        b = bin(int(first, 2) | int(second, 2)).count('1')  # after counting I get max 
         if (b > max):
             max = b
         s.append(str(b))
