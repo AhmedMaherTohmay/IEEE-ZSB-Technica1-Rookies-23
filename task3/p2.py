2aabd3b00c28be8d70047b3edbf0089b83f4e45b
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2022-12-15 11:39:42 +0200

    Update p2.py

--- task3/p2.py
+++ task3/p2.py
@@ -1,25 +1,32 @@
-n = int(input("Enter number of students: "))
-print("Enter the students names and their degrees: ")
+n = int(input())
 stud = []
-for i in range(n):
+for i in range(n):             # getting input
         name = str(input())
         degree = float(input())
         stud.append([name, degree])
 
-for i in range(n - 1):
+for i in range(n - 1):               # comparing each input to sort them
         for j in range(i + 1, n):
             if stud[j][1] < stud[i][1]:
                 switch = stud[i]
                 stud[i] = stud[j]
                 stud[j] = switch
-last2 = 0
+
 sort = []
-for i in range(n):
-    if stud[i][1] == stud[1][1]:
+sort2 = []
+stud.sort
+for i in range(1, n):               # appending numbers ecept first one in list
+    if stud[i][1] != stud[0][1]:
         last2 = stud[i][1]
         sort.append(stud[i])
 
-sort.sort()
-for i in range(len(sort)):
-    print(sort[i][0])
 
+for i in range(1, len(sort)):        # comparing them again but this time with second one
+    if sort[i][1] == sort[0][1]:     #  as I already deleted the first one
+        sort2.append(sort[i][0])
+
+sort2.append(sort[0][0])              # not forgetting to add the second one in case if not 
+                                      # finding any similar's
+sort2.sort()
+for i in range(len(sort2)):
+    print(sort2[i])
